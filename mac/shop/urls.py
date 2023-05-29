81e970088d529b8255265dbc3abae6b1a2b832aa
hemesh3 <82264095+hemesh3@users.noreply.github.com>
2023-05-29 20:28:38 +0530

    only a hotel site

--- mac/shop/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index,name='shop_index'),
-    path("about", views.about,name='shop_about'),
-    path("cart", views.cart,name='shop_cart'),
-]
